0ec73950eda95542edffe7aa8b04b84b51470845
mateuszpob <mateuszpob@gmail.com>
2025-01-30 23:07:56 +0100

    red dealyed

--- main.c
+++ main.c
@@ -4,28 +4,31 @@
 #define RED_PIN PB0
 #define GREEN_PIN PB1
 #define BLUE_PIN PB2
-#define BUTTON_PIN PB3
 
 void setup() {
     // Ustawienie pinów RGB jako wyjścia
     DDRB |= (1 << RED_PIN) | (1 << GREEN_PIN) | (1 << BLUE_PIN);
 
-    // Ustawienie pinu przycisku jako wejście z rezystorem pull-up
-    DDRB &= ~(1 << BUTTON_PIN); // Ustaw jako wejście
-    PORTB |= (1 << BUTTON_PIN); // Włącz pull-up
+    // Konfiguracja PWM dla pinu RED_PIN (PB0) - Timer0
+    TCCR0A = (1 << WGM00) | (1 << WGM01) | (1 << COM0A1); // Fast PWM, wyjście na OC0A (PB0)
+    TCCR0B = (1 << CS01); // Preskaler 8
+
+    // Wyłącz pozostałe kolory
+    PORTB &= ~((1 << GREEN_PIN) | (1 << BLUE_PIN));
 }
 
 int main() {
     setup();
 
-    // Włącz kolor czerwony (RED_PIN na HIGH, pozostałe na LOW)
-    PORTB |= (1 << RED_PIN) | (1 << GREEN_PIN) | (1 << BLUE_PIN);
-    // PORTB &= ~((1 << BLUE_PIN));
+    // Ustaw jasność czerwonego koloru (wartość 0-255, gdzie 255 = maksymalna jasność)
+    OCR0A = 0; // Połowa jasności (możesz zmienić na np. 255 dla pełnej jasności)
 
-    // Główna pętla - nic nie robi, tylko utrzymuje kolor
+    // Główna pętla - nic nie robi poza utrzymaniem ustawień PWM
     while (1) {
-        // Możesz tutaj dodać dodatkowe testy, np. miganie diodą
-        _delay_ms(500);
+        for (uint8_t i = 0; i < 255; i++) {
+            OCR0A = i;       // Zwiększ jasność
+            _delay_ms(5);   // Opóźnienie
+        }
     }
 
     return 0;
