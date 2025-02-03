90d09c27d30358935dab693fc6ff03e2e078b9dd
mateuszpob <mateuszpob@gmail.com>
2025-02-03 21:29:02 +0100

    huj wie co

--- main.c
+++ main.c
@@ -1,20 +1,15 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <math.h>
 
 // Definicje pinów
 #define RED_PIN PB0
-#define GREEN_PIN PB1
-#define BLUE_PIN PB2
-
 #define SW0_PIN PB4
-#define SW1_PIN PB3
 
-volatile uint8_t red_state = 0;
-volatile uint8_t blue_state = 0;
+volatile uint8_t fade_active = 0; // Flaga do sterowania rozjaśnianiem
 
 void delay_ms(uint16_t ms) {
     while (ms--) {
-        // Ta pętla zajmuje ok. 1 ms dla 8 MHz
         for (uint16_t i = 0; i < 400; i++) {
             __asm__ __volatile__("nop");
         }
@@ -22,55 +17,64 @@ void delay_ms(uint16_t ms) {
 }
 
 void setup() {
-    // Ustawienie diod jako wyjścia
-    DDRB |= (1 << RED_PIN) | (1 << BLUE_PIN);
+    // Ustawienie diody jako wyjście
+    DDRB |= (1 << RED_PIN);
 
-    // Przyciski jako wejścia z pull-up
-    DDRB &= ~((1 << SW0_PIN) | (1 << SW1_PIN));
-    PORTB |= (1 << SW0_PIN) | (1 << SW1_PIN);
+    // Konfiguracja PWM dla RED_PIN (PB0) - Timer0
+    TCCR0A = (1 << WGM00) | (1 << WGM01) | (1 << COM0A1); // Fast PWM, wyjście na OC0A
+    TCCR0B = (1 << CS01); // Preskaler 8
 
-    // Włączenie przerwań od zmiany stanu pinów (PCINT)
-    GIMSK |= (1 << PCIE);        // Włącz przerwania dla PCINT
-    PCMSK |= (1 << SW0_PIN) | (1 << SW1_PIN); // Włącz przerwanie dla PB4 i PB3
-    sei();  // Globalne włączenie przerwań
+    // Przycisk jako wejście z pull-up
+    DDRB &= ~(1 << SW0_PIN);
+    PORTB |= (1 << SW0_PIN);
+
+    // Włączenie przerwań od zmiany stanu SW0 (PCINT)
+    GIMSK |= (1 << PCIE);
+    PCMSK |= (1 << SW0_PIN);
+    sei(); // Globalne włączenie przerwań
 }
 
 void fade(uint16_t total_time, float easing_factor) {
-    uint16_t steps = 255; // Ilość kroków w cyklu (8-bitowy PWM)
-    uint16_t delay_per_step = total_time / steps; // Czas na jeden krok
+    uint16_t steps = 255;
+    uint16_t delay_per_step = total_time / steps;
 
     for (uint16_t i = 0; i < steps; i++) {
-        // Funkcja easing: zastosowanie potęgi (easing_factor) do wartości i
-        float normalized = (float)i / steps; // Normalizacja do zakresu 0-1
+        if (!fade_active) return; // Jeśli przycisk zostanie wciśnięty, natychmiast przerwij
+
+        float normalized = (float)i / steps;
         uint8_t brightness = (uint8_t)(pow(normalized, easing_factor) * 255);
 
         OCR0A = brightness; // Ustaw jasność
-        delay_ms(delay_per_step); // Opóźnienie dla czasu całkowitego
+        delay_ms(delay_per_step);
+    }
+}
+
+// Przerwanie od zmiany stanu na SW0
+ISR(PCINT0_vect) {
+    delay_ms(50); // Proste debounce
+    if (!(PINB & (1 << SW0_PIN))) { // SW0 wciśnięty
+        fade_active = 0; // Przerwij rozjaśnianie
+        TCCR0A &= ~((1 << COM0A1) | (1 << COM0A0)); // Wyłącz PWM
+        PORTB &= ~(1 << RED_PIN); // Wymuś stan niski
+    } else { // SW0 puszczony
+        fade_active = 1; // Aktywuj rozjaśnianie
     }
 }
 
 int main() {
     setup();
+    if (!(PINB & (1 << SW0_PIN))) { // SW0 wciśnięty
+        fade_active = 0; // Przerwij rozjaśnianie
+        TCCR0A &= ~((1 << COM0A1) | (1 << COM0A0)); // Wyłącz PWM
+        PORTB &= ~(1 << RED_PIN); // Wymuś stan niski
+    } else { // SW0 puszczony
+        fade_active = 1; // Aktywuj rozjaśnianie
+    }
 
     while (1) {
-        // Nic nie robimy, obsługa przycisków odbywa się w przerwaniach
-    }
-}
-
-// Przerwanie od zmiany stanu na pinach
-ISR(PCINT0_vect) {
-    // Debounce - krótka zwłoka
-    delay_ms(200);
-    
-    if (!(PINB & (1 << SW0_PIN))) { // Jeśli SW0 (PB4) wciśnięty
-        red_state ^= 1; // Zmiana stanu (toggle)
-        if (red_state) PORTB |= (1 << RED_PIN); // Włącz czerwoną diodę
-        else PORTB &= ~(1 << RED_PIN); // Wyłącz czerwoną diodę
-    }
-
-    if (!(PINB & (1 << SW1_PIN))) { // Jeśli SW1 (PB3) wciśnięty
-        blue_state ^= 1; // Zmiana stanu (toggle)
-        if (blue_state) PORTB |= (1 << BLUE_PIN); // Włącz niebieską diodę
-        else PORTB &= ~(1 << BLUE_PIN); // Wyłącz niebieską diodę
+        if (fade_active) {
+            fade(1000, 2.0);
+            // fade_active = 0; // Zapobiega wielokrotnemu uruchamianiu w pętli
+        }
     }
 }
